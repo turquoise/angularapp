f4daafdab4cee9712ea02a224344a6c0381f1d9b
M Gosschalk <monica.gosschalk@btinternet.com>
2017-10-01 17:32:28 +0100

    added routermodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SandboxComponent } from './components/sandbox/sandbox.component';
@@ -10,6 +11,16 @@ import { GraphComponent } from './components/graph/graph.component';
 import { HttpComponent } from './components/http/http.component';
 import { DataService } from './services/data.service';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'user/:id', component: UserDetailsComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -18,12 +29,16 @@ import { NavComponent } from './components/nav/nav.component';
     FormsComponent,
     GraphComponent,
     HttpComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     DataService
